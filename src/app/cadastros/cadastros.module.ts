d1b86619b09bf9fb660114e084b4d908136055cd
Miguel Jose Albano da Silva <61022078+josemiguel2004@users.noreply.github.com>
2022-09-06 08:55:54 -0300

    Criação da Validação do Cadastro

--- src/app/cadastros/cadastros.module.ts
+++ src/app/cadastros/cadastros.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { CadastrosRoutingModule } from './cadastros-routing.module';
 import { CadastroComponent } from './cadastro/cadastro.component';
 
@@ -11,7 +13,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
   ],
   imports: [
     CommonModule,
-    CadastrosRoutingModule
+    CadastrosRoutingModule,
+    FormsModule, ReactiveFormsModule
   ]
 })
 export class CadastrosModule { }
